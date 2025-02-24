a69c1193e2b2520e89621265f7606f4bbfaa984d
kiteee1 <168648447+kiteee1@users.noreply.github.com>
2025-02-24 14:08:25 +0100

    Add files via upload

---
+++ smart_pointer/campaign.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include <map>
+#include <stdexcept>
+#include "hero.h"
+#include "campaign.h"
+
+Campaign::Campaign(std::string name, unsigned min_level) : name{name}, min_level{min_level}
+{
+    if (name.empty())
+        throw std::runtime_error("Campaign Name Empty");
+    if (min_level <= 0)
+        throw std::runtime_error("Min Level <=0");
+}
+
+void Campaign::add_hero(std::shared_ptr<Hero> h)
+{
+    if (heroes.find(h->get_id()) != heroes.end() || h->get_level() < min_level)
+        throw std::runtime_error("add_hero error");
+    heroes[h->get_id()] = h;
+}
+
+void Campaign::encounter_monster(unsigned id, Monster &m)
+{
+    auto it = heroes.find(id);
+    if (it == heroes.end())
+        throw std::runtime_error("Hero not in campaign.");
+
+    auto hero = it->second.lock();
+    if (hero && hero->fight(m))
+        hero->level_up();
+}
+
+unsigned Campaign::remove_dead_and_expired()
+{
+    unsigned count = 0;
+    for (auto it = heroes.begin(); it != heroes.end();)
+    {
+        auto hero = it->second.lock();
+        if (!hero || hero->get_current_hp() == 0)
+        {
+            it = heroes.erase(it);
+            ++count;
+        }
+        else
+            ++it;
+    }
+    return count;
+}
+
+std::ostream &Campaign::print(std::ostream &o) const
+{
+    o << "[" << name << " Campaign, Min_Level " << min_level << ", {";
+    bool first = true;
+    for (const auto &pair : heroes)
+    {
+        auto hero = pair.second.lock();
+        if (hero)
+        {
+            if (!first)
+                o << ", ";
+            o << *hero;
+            first = false;
+        }
+    }
+    o << "}]";
+    return o;
+}
+
+std::ostream &operator<<(std::ostream &o, const Campaign &p) { return p.print(o); }
