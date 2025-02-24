1cbaaf9e157694fadea35be226e7b86053489c45
kiteee1 <168648447+kiteee1@users.noreply.github.com>
2025-02-24 14:01:57 +0100

    Add files via upload

---
+++ TrafficController/BridgeGUI.java
@@ -0,0 +1,28 @@
+package assigment2;
+
+public class BridgeGUI {
+
+    private static void nap(int ms) {
+	try {
+	    Thread.sleep(ms);
+	} catch (InterruptedException e) {}
+    }
+
+
+    public static void main(String[] a) {
+	final CarWindow win = new CarWindow();
+
+	win.pack();
+	win.setVisible(true);
+
+	new Thread(new Runnable() {
+		public void run() {
+		    while (true) {
+			nap(25);
+			win.repaint();
+		    }
+		}
+	    }).start();
+    }
+
+}
---
+++ TrafficController/TrafficRegistrar.java
@@ -0,0 +1,8 @@
+package assigment2;
+
+public interface TrafficRegistrar {
+	void registerLeft(Vehicle v);
+	void registerRight(Vehicle v);
+	void deregisterLeft(Vehicle v);
+	void deregisterRight(Vehicle v);
+}
